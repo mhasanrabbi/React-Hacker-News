15f5d92490758d4a4761cfc209ba9f8f51ec4c3d
mahmudhasanrabbi <mdmahmudhasanrabbi@gmail.com>
2021-08-21 22:16:49 +0600

    SET_LOADING

--- src/reducer.js
+++ src/reducer.js
@@ -1,3 +1,11 @@
+import {
+  SET_LOADING
+} from './actions';
 
-const reducer = () => {}
+const reducer = (state,action) => {
+  if(action.type === SET_LOADING) {
+    return {...state, isLoading:true}
+  }
+    throw new Error (`no matching "${action.type}" action type`)
+}
 export default reducer;
