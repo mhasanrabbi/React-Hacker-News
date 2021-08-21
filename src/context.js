14ac8f9b795454a303117a76cf7f5ecc779653fe
mahmudhasanrabbi <mdmahmudhasanrabbi@gmail.com>
2021-08-21 21:12:57 +0600

    context folder

---
+++ src/context.js
@@ -0,0 +1,17 @@
+import React, { useContext } from 'react';
+
+const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?'
+
+const initialState = {}
+
+const AppContext = React.createContext()
+
+const AppProvider = ({ children }) => {
+  return <AppContext.Provider value='hello'>{children}</AppContext.Provider>
+}
+
+export const useGlobalContext = () => {
+  return useContext(AppContext)
+}
+
+export { AppContext, AppProvider };
